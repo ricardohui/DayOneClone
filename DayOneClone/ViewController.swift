c2bf4b4d64982726ad8cf56ff0b6c63cc6563650
Ricardo Hui <eehaha@gmail.com>
2019-06-03 08:06:13 +0800

    Initial Commit

---
+++ DayOneClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayOneClone
+//
+//  Created by Ricardo Hui on 3/6/2019.
+//  Copyright © 2019 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
