6fe624ba04d80f93f33694c1a19a684c6ba998a2
Ricardo Hui <eehaha@gmail.com>
2019-06-08 07:39:49 +0800

    list journal entries

--- DayOneClone/JournalTableViewController.swift
+++ DayOneClone/JournalTableViewController.swift
@@ -13,23 +13,28 @@ class JournalTableViewController: UITableViewController {
 
     @IBOutlet var plusButton: UIButton!
     @IBOutlet var cameraButton: UIButton!
+    
+    var entries :  Results<Entry>? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
         plusButton.imageView?.contentMode = .scaleAspectFit
         cameraButton.imageView?.contentMode = .scaleAspectFit
         
-        if let realm = try? Realm(){
-            let entries = realm.objects(Entry.self)
-            print(entries.count)
-            if entries.count > 0{
-                print(entries[0].text)
-                print(entries[0].date)
-                print(entries[0].pictures.count)
-            }
-        }
+       getEntries()
     }
 
+    func getEntries(){
+        if let realm  = try? Realm(){
+         entries = realm.objects(Entry.self).sorted(byKeyPath: "date", ascending: false)
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getEntries()
+    }
     
     @IBAction func cameraTapped(_ sender: Any) {
         performSegue(withIdentifier: "goToNew", sender: "camera")
@@ -42,14 +47,39 @@ class JournalTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        if let entries = self.entries{
+            return entries.count
+        }else{
+                return 0
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as? JournalCell{
+            if let entry = entries?[indexPath.row]{
+                cell.previewTextLabel.text = entry.text
+                if let image = entry.pictures.first?.thumbnail(){
+                    cell.imageViewWidth.constant = 100
+                    cell.previewImageView.image = image
+                }else{
+                    cell.imageViewWidth.constant = 0
+                }
+            }
+            
+            
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
@@ -63,4 +93,17 @@ class JournalTableViewController: UITableViewController {
         }
     }
 
+}
+
+class JournalCell : UITableViewCell{
+    
+    @IBOutlet var previewTextLabel: UILabel!
+    @IBOutlet var previewImageView: UIImageView!
+
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var monthLabel: UILabel!
+    @IBOutlet var yearLabel: UILabel!
+
+    @IBOutlet var imageViewWidth: NSLayoutConstraint!
+    
 }
